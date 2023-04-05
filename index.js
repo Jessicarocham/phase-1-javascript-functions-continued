0e5bf22863324f6809824e61e04aec0898ad894e
jessicarocham <jessicarocham18@hotmail.com>
2023-04-05 11:53:40 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+saturdayFun = (activity = "roller-skate") =>
+  `This Saturday, I want to ${activity}!`;
+
+saturdayFun();
+saturdayFun("bathe my dog");
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+function wrapAdjective(param = "*") {
+  const part1 = "You are";
+  return function innerFunction(param2 = "special") {
+    return `${part1} ${param}${param2}${param}!`;
+  };
+}
